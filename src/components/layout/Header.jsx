6af1ffff8acf77cbd710d4c9b2de275520b4e917
Rahul Bisht <rahulbisht8886@gmail.com>
2025-08-14 16:53:45 +0530

    used nav links

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav className="bg-gray-800 text-white p-4">
+      <div className="flex justify-between items-center">
+        {/* Left Side: Name */}
+        <div className="text-2xl font-bold">benevolent</div>
 
-export default Header
+        {/* Right Side: Navigation Links */}
+        <div className="flex space-x-6">
+          <NavLink to="/" className="hover:text-gray-300">
+            Home
+          </NavLink>
+          <NavLink to="/about" className="hover:text-gray-300">
+            About
+          </NavLink>
+          <NavLink to="/contact" className="hover:text-gray-300">
+            Contact
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
