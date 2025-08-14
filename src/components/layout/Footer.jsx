f29444206799a05cf708136e6bdf62c8b70281b0
Rahul Bisht <rahulbisht8886@gmail.com>
2025-08-14 17:00:48 +0530

    updated pages

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-gray-800 text-white p-4">
+      <div className="text-center text-sm">
+        <p>&copy; 2025 benevolent. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
