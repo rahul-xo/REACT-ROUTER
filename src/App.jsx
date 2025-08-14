a6e220a8982d34b67cb2eb7387e3818da8efb4ea
Rahul Bisht <rahulbisht8886@gmail.com>
2025-08-14 15:05:51 +0530

    work with router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router-dom'
 
 const App = () => {
+  const router=createBrowserRouter([
+    {
+      path:"/",
+      element: <div className="">Home Page</div>
+    },
+    {
+      path:"/contact",
+      element: <div className="">Contact us</div>
+    },
+    {
+      path:"/About",
+      element: <div className="">About us</div>
+    },
+  ])
   return (
-    <div>App</div>
+    <RouterProvider router={router}/>
   )
 }
 
