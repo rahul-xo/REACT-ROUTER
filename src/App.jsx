a8dd20a1b7fed833f7d4cd7f13045f3a2c460460
Rahul Bisht <rahulbisht8886@gmail.com>
2025-08-14 16:31:28 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
-import { createBrowserRouter } from 'react-router'
-import { RouterProvider } from 'react-router-dom'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import AppUi from "./components/layout/AppUi";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+
+
 
 const App = () => {
-  const router=createBrowserRouter([
+  const router = createBrowserRouter([
     {
       path:"/",
-      element: <div className="">Home Page</div>
-    },
-    {
-      path:"/contact",
-      element: <div className="">Contact us</div>
-    },
-    {
-      path:"/About",
-      element: <div className="">About us</div>
-    },
-  ])
-  return (
-    <RouterProvider router={router}/>
-  )
-}
+      element: <AppUi/>,
+      children: [
+        {
+          path:"/",
+          element: <Home/>
+        },
+        {
+          path:"/contact",
+          element: <Contact/>,
+        },
+        {
+          path:"/about",
+          element: <About/>
+        },
+      ]
+    }
+  ]);
+  return <RouterProvider router={router}/>;
+};
 
-export default App
+export default App;
